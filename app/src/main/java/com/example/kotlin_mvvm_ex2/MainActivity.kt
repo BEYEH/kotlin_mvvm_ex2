6abcb836b74a1e8f68ba9af9e9296e1beb910446
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-14 13:57:12 +0800

    Update main activity.

--- app/src/main/java/com/example/kotlin_mvvm_ex2/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_mvvm_ex2/MainActivity.kt
@@ -20,28 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Kotlin_mvvm_ex2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Text(
+                        text = "Hello!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Kotlin_mvvm_ex2Theme {
-        Greeting("Android")
     }
 }
